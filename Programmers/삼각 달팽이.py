3cf32d2534cbb4f7cd16baa3a8e328b38aaecd7f
Jiyoon Jung <76610641+jy9922@users.noreply.github.com>
2023-01-17 15:51:57 +0900

    2023.01.17 프로그래머스 문제풀이 완
    https://school.programmers.co.kr/learn/courses/30/lessons/68645

---
+++ Programmers/삼각 달팽이.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = [[0] * x for x in range(1, n+1)]
+    x, y = -1, 0
+    cnt = 1
+    
+    for i in range(n):
+        for j in range(i, n):
+            if i % 3 == 0:
+                x += 1
+            elif i % 3 == 1:
+                y += 1
+            else:
+                x -= 1
+                y -= 1
+            answer[x][y] = cnt
+            cnt += 1
+    return (sum(answer, []))
